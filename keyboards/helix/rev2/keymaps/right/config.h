e672f98f397b0d1ff261e3e77816d1f4106b9264
Yuki Koya <ykky@pepabo.com>
2018-03-13 20:56:10 +0900

    意味なさそうなのでけす

--- keyboards/helix/rev2/keymaps/right/config.h
+++ keyboards/helix/rev2/keymaps/right/config.h
@@ -63,7 +63,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PS2_DATA_DDR    DDRB
 #define PS2_DATA_BIT    6
 
-#define PS2_MOUSE_USE_REMOTE_MODE
 #define PS2_MOUSE_X_MULTIPLIER 3
 #define PS2_MOUSE_Y_MULTIPLIER 3
 #define PS2_MOUSE_V_MULTIPLIER 1
