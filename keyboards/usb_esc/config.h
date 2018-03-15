30abbe6ef7c68404e7871bfea4cfbb635622ea1e
Yuki Koya <ykky@pepabo.com>
2018-03-15 18:30:57 +0900

    USB-ESCを追加

---
+++ keyboards/usb_esc/config.h
@@ -0,0 +1,21 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "config_common.h"
+
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0xE53C
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    buty4649
+#define PRODUCT         USB-ESC key
+#define DESCRIPTION     USB ESC KEY
+
+#define MATRIX_ROWS 1
+#define MATRIX_COLS 1
+
+#define MATRIX_COL_PINS { F5 }
+#define MATRIX_ROW_PINS { B4 }
+#define UNUSED_PINS
+
+
+#endif
---
+++ keyboards/usb_esc/usb_esc.h
@@ -0,0 +1,13 @@
+#ifndef USB_ESC_H
+#define USB_ESC_H
+
+#include "quantum.h"
+
+#define KEYMAP( \
+        k00     \
+) \
+{ \
+    { k00 } \
+}
+
+#endif
